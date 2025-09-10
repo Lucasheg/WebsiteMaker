ca13581185f9e6096e306f0d28d9f245c5fca4d3
Lucasheg <contact@citeks.net>
2025-09-10 19:30:20 +0200

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Maker from "./Maker.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Maker />
-  </React.StrictMode>
-);
